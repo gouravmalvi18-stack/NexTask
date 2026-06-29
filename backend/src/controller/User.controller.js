ab42ded549dd8c4fac342bcec5d07656c4448941
Gaurav_Malvi <gouravmalvi18@gmail.com>
2026-06-29 17:38:44 +0530

    fix backend bug

--- backend/src/controller/User.controller.js
+++ backend/src/controller/User.controller.js
@@ -41,7 +41,11 @@ const CreateUser = async (req, res) => {
       process.env.JWT_SECRET_KEY,
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
     res.status(201).json({
       NewUser: NewUser,
     });
@@ -83,7 +87,12 @@ const LoginUser = async (req, res) => {
       process.env.JWT_SECRET_KEY,
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
     res.status(200).json({
       LoginUser: User,
     });
